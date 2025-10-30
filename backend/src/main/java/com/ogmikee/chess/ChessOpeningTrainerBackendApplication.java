477d0172e495de860fb94aa06d69edc6ef1cec9d
Mickael Gatti <mickael.gatti@epita.fr>
2025-10-30 22:39:46 +0100

    build(backend): add spring boot app entry point

---
+++ backend/src/main/java/com/ogmikee/chess/ChessOpeningTrainerBackendApplication.java
@@ -0,0 +1,14 @@
+/**package com.ogmikee.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessOpeningTrainerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessOpeningTrainerBackendApplication.class, args);
+	}
+
+}
+**/
