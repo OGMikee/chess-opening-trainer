1c2bf5702e4b860e179bec575766bcc11b4c9de1
Mickael Gatti <mickael.gatti@epita.fr>
2025-10-30 22:37:20 +0100

    build(backend): initialize sprint boot project w maven

---
+++ backend/src/test/java/com/ogmikee/chess/ChessOpeningTrainerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogmikee.chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessOpeningTrainerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
